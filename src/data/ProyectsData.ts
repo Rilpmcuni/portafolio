990a20d3cd7243cec42b4eddca9368ffd8425daa
Fabian Lisantti <134806143+Rilpmcuni@users.noreply.github.com>
2023-06-01 02:46:36 +0000

    Update ProyectsData.ts

--- src/data/ProyectsData.ts
+++ src/data/ProyectsData.ts
@@ -6,17 +6,21 @@ import proyectMemeate from "@/src/assets/proyectMemeate.png";
 
 export const ProyectsData = [
   {
-    title: "Muy pronto",
-    page: "Muy pronto",
-    badge: ["?"],
-    description: "¿Quién sabe que vendrá despues?...",
-    bgProyect: [proyect],
+    title: "Tamíz.LA",
+    page: "tamiz la",
+    badge: ["Next.js", "Tailwindcss", "Supabase"],
+    description:
+      "Un proyecto ambicioso que llevo desarrollando desde hace un tiempo.",
+    bgProyect: [proyectTamizLA],
     bgCard: [
-      "https://github.com/Incmplir/landing-page/blob/main/proyect-0.png?raw=true",
+      "https://github.com/Incmplir/landing-page/blob/main/proyectTamizLA-0.png?raw=true",
     ],
     parraph:
-      "Muy pronto, estaré lanzando nuevos proyectos y demostrando mi habilidad para crear experiencias en línea únicas y entretenidas. Además, podrás ver mi capacidad para diseñar y desarrollar aplicaciones empresariales complejas, y cómo las tecnologías como Next.js, Tailwind CSS y Firebase me permiten crear soluciones tecnológicas innovadoras. ¡Manténte atento para descubrir más acerca de mi trabajo y cómo puedo ayudar a llevar tus ideas al siguiente nivel!",
-    links: ["http://github.com/incmplir/", "http://github.com/incmplir/"],
+      "Proyecto TamizLA - Este es un proyecto ambicioso que he estado desarrollando durante algún tiempo. TamizLA es una solución tecnológica para profesionales laboratoristas que buscan mejorar la eficiencia en su trabajo. Al utilizar tecnologías modernas como Next.js, Tailwind CSS, Auth0 y Firebase, he creado un sistema que incluye características como material de estudio, seguimiento de ensayos, informes mensuales y más. Con TamizLA, los profesionales laboratoristas pueden ahorrar tiempo y aumentar la precisión en su trabajo, lo que resulta en una mayor satisfacción del cliente y una mayor eficiencia en general. Estoy emocionado de agregar TamizLA a mi portafolio, ya que demuestra mi capacidad para crear soluciones tecnológicas útiles y efectivas para problemas del mundo real.",
+    links: [
+      "http://tamiz-la.vercel.app/",
+      "https://github.com/Incmplir/tamizla-next",
+    ],
   },
   {
     title: "Bebé",
@@ -32,6 +36,22 @@ export const ProyectsData = [
     links: [
       "https://bebe-vite.vercel.app/",
       "https://github.com/Incmplir/bebe-vite",
+    ],
+  },
+  {
+    title: "Memeate",
+    page: "memeate",
+    badge: ["Vite", "Tailwindcss", "CodeSandbox"],
+    description: "Otro proyecto gracioso desarrollado por los memes.",
+    bgProyect: [proyectMemeate],
+    bgCard: [
+      "https://github.com/Rilpmcuni/portafolio/blob/sandbox/src/assets/proyectMemeate.png?raw=true",
+    ],
+    parraph:
+      "Proyecto Memeate - Desarrollado con pasión y creatividad en mis ratos libres, Memeate es un proyecto divertido que te permite crear y descargar memes personalizados de forma sencilla. Utilizando herramientas como Vite, Tailwind CSS y CodeSandbox, este proyecto ofrece una experiencia interactiva donde los usuarios pueden seleccionar plantillas de memes populares, agregar su propio texto personalizado y luego descargar los memes creados. Al ingresar al sitio, los usuarios son recibidos con una interfaz intuitiva y amigable. Se les presenta una selección de plantillas de memes, organizadas por categorías para facilitar la búsqueda. Al elegir una plantilla, se muestra una vista previa del meme con los campos de texto correspondientes. Los usuarios pueden personalizar el texto, agregar sus propias ideas ingeniosas y crear memes únicos. Una vez que los usuarios han creado su meme personalizado, tienen la opción de descargarlo en su dispositivo. Memeate proporciona un botón de descarga que les permite guardar el meme como una imagen en formato PNG o JPEG. Esto permite a los usuarios compartir sus memes en redes sociales, enviarlos a amigos o simplemente guardarlos como recuerdo. Además de la función de creación y descarga de memes, el proyecto Memeate cuenta con una dosis adicional de humor. A lo largo de la experiencia, se incluyen chistes entretenidos y divertidos, que mantienen el ambiente ligero y entretenido. Estos chistes se presentan de forma interactiva, sorprendiendo a los usuarios en momentos estratégicos y añadiendo un toque de diversión extra a la experiencia de creación de memes. El uso de CodeSandbox como entorno de desarrollo permite una colaboración sencilla y un fácil acceso al proyecto. Esto significa que los usuarios pueden disfrutar de Memeate en línea, sin necesidad de instalar nada en su dispositivo. La combinación de Vite y Tailwind CSS asegura un rendimiento óptimo y un diseño moderno y atractivo, proporcionando una experiencia de usuario fluida y agradable. En resumen, el proyecto Memeate es una adición creativa y entretenida a mi portafolio. Muestra mi habilidad para desarrollar aplicaciones web divertidas y útiles, aprovechando herramientas como Vite, Tailwind CSS y CodeSandbox. Además, la inclusión de chistes, la capacidad de personalizar los memes y la opción de descargarlos ofrecen una experiencia interactiva y agradable para los usuarios. Memeate es el lugar perfecto para dejar volar la imaginación, crear memes que despierten risas y tener la posibilidad de guardarlos como recuerdos o compartirlos con el mundo.",
+    links: [
+      "https://memeate-rilpmcuni.vercel.app/",
+      "https://github.com/Rilpmcuni/Memeate",
     ],
   },
   {
@@ -52,36 +72,16 @@ export const ProyectsData = [
     ],
   },
   {
-    title: "Tamíz.LA",
-    page: "tamiz la",
-    badge: ["Next.js", "Tailwindcss", "Supabase"],
-    description:
-      "Un proyecto ambicioso que llevo desarrollando desde hace un tiempo.",
-    bgProyect: [proyectTamizLA],
+    title: "Muy pronto",
+    page: "Muy pronto",
+    badge: ["?"],
+    description: "¿Quién sabe que vendrá despues?...",
+    bgProyect: [proyect],
     bgCard: [
-      "https://github.com/Incmplir/landing-page/blob/main/proyectTamizLA-0.png?raw=true",
+      "https://github.com/Incmplir/landing-page/blob/main/proyect-0.png?raw=true",
     ],
     parraph:
-      "Proyecto TamizLA - Este es un proyecto ambicioso que he estado desarrollando durante algún tiempo. TamizLA es una solución tecnológica para profesionales laboratoristas que buscan mejorar la eficiencia en su trabajo. Al utilizar tecnologías modernas como Next.js, Tailwind CSS, Auth0 y Firebase, he creado un sistema que incluye características como material de estudio, seguimiento de ensayos, informes mensuales y más. Con TamizLA, los profesionales laboratoristas pueden ahorrar tiempo y aumentar la precisión en su trabajo, lo que resulta en una mayor satisfacción del cliente y una mayor eficiencia en general. Estoy emocionado de agregar TamizLA a mi portafolio, ya que demuestra mi capacidad para crear soluciones tecnológicas útiles y efectivas para problemas del mundo real.",
-    links: [
-      "http://tamiz-la.vercel.app/",
-      "https://github.com/Incmplir/tamizla-next",
-    ],
-  },
-  {
-    title: "Memeate",
-    page: "memeate",
-    badge: ["Vite", "Tailwindcss", "CodeSandbox"],
-    description: "Otro proyecto Gracioso desarrollado por los memes.",
-    bgProyect: [proyectMemeate],
-    bgCard: [
-      "https://github.com/Rilpmcuni/portafolio/blob/sandbox/src/assets/proyectMemeate.png?raw=true",
-    ],
-    parraph:
-      "Proyecto Memeate - Desarrollado con pasión y creatividad en mis ratos libres, Memeate es un proyecto divertido que te permite crear y descargar memes personalizados de forma sencilla. Utilizando herramientas como Vite, Tailwind CSS y CodeSandbox, este proyecto ofrece una experiencia interactiva donde los usuarios pueden seleccionar plantillas de memes populares, agregar su propio texto personalizado y luego descargar los memes creados. Al ingresar al sitio, los usuarios son recibidos con una interfaz intuitiva y amigable. Se les presenta una selección de plantillas de memes, organizadas por categorías para facilitar la búsqueda. Al elegir una plantilla, se muestra una vista previa del meme con los campos de texto correspondientes. Los usuarios pueden personalizar el texto, agregar sus propias ideas ingeniosas y crear memes únicos. Una vez que los usuarios han creado su meme personalizado, tienen la opción de descargarlo en su dispositivo. Memeate proporciona un botón de descarga que les permite guardar el meme como una imagen en formato PNG o JPEG. Esto permite a los usuarios compartir sus memes en redes sociales, enviarlos a amigos o simplemente guardarlos como recuerdo. Además de la función de creación y descarga de memes, el proyecto Memeate cuenta con una dosis adicional de humor. A lo largo de la experiencia, se incluyen chistes entretenidos y divertidos, que mantienen el ambiente ligero y entretenido. Estos chistes se presentan de forma interactiva, sorprendiendo a los usuarios en momentos estratégicos y añadiendo un toque de diversión extra a la experiencia de creación de memes. El uso de CodeSandbox como entorno de desarrollo permite una colaboración sencilla y un fácil acceso al proyecto. Esto significa que los usuarios pueden disfrutar de Memeate en línea, sin necesidad de instalar nada en su dispositivo. La combinación de Vite y Tailwind CSS asegura un rendimiento óptimo y un diseño moderno y atractivo, proporcionando una experiencia de usuario fluida y agradable. En resumen, el proyecto Memeate es una adición creativa y entretenida a mi portafolio. Muestra mi habilidad para desarrollar aplicaciones web divertidas y útiles, aprovechando herramientas como Vite, Tailwind CSS y CodeSandbox. Además, la inclusión de chistes, la capacidad de personalizar los memes y la opción de descargarlos ofrecen una experiencia interactiva y agradable para los usuarios. Memeate es el lugar perfecto para dejar volar la imaginación, crear memes que despierten risas y tener la posibilidad de guardarlos como recuerdos o compartirlos con el mundo.",
-    links: [
-      "https://memeate-rilpmcuni.vercel.app/",
-      "https://github.com/Rilpmcuni/Memeate",
-    ],
+      "Muy pronto, estaré lanzando nuevos proyectos y demostrando mi habilidad para crear experiencias en línea únicas y entretenidas. Además, podrás ver mi capacidad para diseñar y desarrollar aplicaciones empresariales complejas, y cómo las tecnologías como Next.js, Tailwind CSS y Firebase me permiten crear soluciones tecnológicas innovadoras. ¡Manténte atento para descubrir más acerca de mi trabajo y cómo puedo ayudar a llevar tus ideas al siguiente nivel!",
+    links: ["http://github.com/incmplir/", "http://github.com/incmplir/"],
   },
 ];
