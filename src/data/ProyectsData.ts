1649b7c7bbc4f597078088d2c55e466338b03666
Fabian Lisantti <134806143+Rilpmcuni@users.noreply.github.com>
2023-06-01 02:35:00 +0000

    Update 3 files

--- src/data/ProyectsData.ts
+++ src/data/ProyectsData.ts
@@ -2,68 +2,84 @@ import proyect from "@/src/assets/proyect-0.png";
 import proyectBebe from "@/src/assets/proyectBebe-0.png";
 import proyectPortafolio from "@/src/assets/proyectPortafolio-0.png";
 import proyectTamizLA from "@/src/assets/proyectTamizLA-0.png";
+import proyectMemeate from "@/src/assets/proyectMemeate.png";
 
 export const ProyectsData = [
-    {
-        title: "Muy pronto",
-        page: "Muy pronto",
-        badge: ["?"],
-        description: "¿Quién sabe que vendrá despues?...",
-        bgProyect: [proyect],
-        bgCard: [
-            "https://github.com/Incmplir/landing-page/blob/main/proyect-0.png?raw=true",
-        ],
-        parraph:
-            "Muy pronto, estaré lanzando nuevos proyectos y demostrando mi habilidad para crear experiencias en línea únicas y entretenidas. Además, podrás ver mi capacidad para diseñar y desarrollar aplicaciones empresariales complejas, y cómo las tecnologías como Next.js, Tailwind CSS y Firebase me permiten crear soluciones tecnológicas innovadoras. ¡Manténte atento para descubrir más acerca de mi trabajo y cómo puedo ayudar a llevar tus ideas al siguiente nivel!",
-        links: ["http://github.com/incmplir/", "http://github.com/incmplir/"],
-    },
-    {
-        title: "Bebé",
-        page: "bebe",
-        badge: ["vite", "Tailwindcss"],
-        description: "Un proyecto personal desarrollado solo por diversión.",
-        bgProyect: [proyectBebe],
-        bgCard: [
-            "https://github.com/Incmplir/landing-page/blob/main/proyectBebe-0.png?raw=true",
-        ],
-        parraph:
-            "Proyecto Bebé - Desarrollado por pura diversión en mis ratos libres, Bebé es un proyecto chistoso que muestra a dos bebés luchando en un video 3D mientras una canción emocionante suena de fondo. Al principio, los usuarios son recibidos con una pantalla en negro simple con un botón en el centro. Al hacer clic en el botón, el video 3D aparece con dos bebés luchando mientras otros bebés bailan en el fondo. Este proyecto fue desarrollado usando Vite y Tailwind CSS para crear la pantalla inicial y agregar estilo a la página en general. La simplicidad de la pantalla inicial aumenta el efecto cómico del video 3D que aparece después. Si bien este proyecto fue creado por pura diversión, demuestra mi habilidad para usar herramientas de desarrollo para crear una experiencia única en línea. Además, muestra mi capacidad para crear un proyecto chistoso y entretenido que puede atraer a un público amplio. En resumen, el proyecto Bebé es una adición divertida y creativa a mi portafolio. Muestra mi habilidad para crear una experiencia en línea única y entretenida utilizando herramientas de desarrollo como Vite y Tailwind CSS.",
-        links: [
-            "https://bebe-vite.vercel.app/",
-            "https://github.com/Incmplir/bebe-vite",
-        ],
-    },
-    {
-        title: "Portafolio",
-        page: "portafolio",
-        badge: ["Next.js 13", "Tailwindcss"],
-        description: "Mi primer proyecto actualizado constantemente con nuevas tecnologías.",
-        bgProyect: [proyectPortafolio],
-        bgCard: [
-            "https://github.com/Incmplir/landing-page/blob/main/proyectPortafolio-0.png?raw=true",
-        ],
-        parraph:
-            "Mi portafolio es una muestra de mi habilidad para crear aplicaciones web dinámicas y responsivas utilizando las últimas tecnologías de desarrollo. Construido con Next.js y Tailwindcss, presenta una interfaz de usuario limpia y moderna que se adapta perfectamente a cualquier dispositivo. Además, gracias a la implementación de páginas dinámicas y el fetching de datos, los usuarios pueden navegar sin problemas a través de diferentes secciones del sitio y acceder a información detallada sobre mis proyectos. Entre ellos se encuentran Bebé, un proyecto chistoso que demuestra mi capacidad para crear experiencias en línea únicas y entretenidas, y TamizLA, una solución tecnológica para profesionales laboratoristas que destaca mi habilidad para diseñar y desarrollar aplicaciones empresariales complejas. En resumen, mi portafolio es una muestra del alcance y la calidad de mi trabajo como desarrollador web.",
-        links: [
-            "http://christian-lisantti.netlify.app/",
-            "https://github.com/Incmplir/portfolio-next",
-        ],
-    },
-    {
-        title: "Tamíz.LA",
-        page: "tamiz la",
-        badge: ["Next.js", "Tailwindcss", "Supabase"],
-        description:
-            "Un proyecto ambicioso que llevo desarrollando desde hace un tiempo.",
-        bgProyect: [proyectTamizLA],
-        bgCard: [
-            "https://github.com/Incmplir/landing-page/blob/main/proyectTamizLA-0.png?raw=true",
-        ],
-        parraph:
-            "Proyecto TamizLA - Este es un proyecto ambicioso que he estado desarrollando durante algún tiempo. TamizLA es una solución tecnológica para profesionales laboratoristas que buscan mejorar la eficiencia en su trabajo. Al utilizar tecnologías modernas como Next.js, Tailwind CSS, Auth0 y Firebase, he creado un sistema que incluye características como material de estudio, seguimiento de ensayos, informes mensuales y más. Con TamizLA, los profesionales laboratoristas pueden ahorrar tiempo y aumentar la precisión en su trabajo, lo que resulta en una mayor satisfacción del cliente y una mayor eficiencia en general. Estoy emocionado de agregar TamizLA a mi portafolio, ya que demuestra mi capacidad para crear soluciones tecnológicas útiles y efectivas para problemas del mundo real.",
-        links: [
-            "http://tamiz-la.vercel.app/",
-            "https://github.com/Incmplir/tamizla-next",
-        ],
-    },
+  {
+    title: "Muy pronto",
+    page: "Muy pronto",
+    badge: ["?"],
+    description: "¿Quién sabe que vendrá despues?...",
+    bgProyect: [proyect],
+    bgCard: [
+      "https://github.com/Incmplir/landing-page/blob/main/proyect-0.png?raw=true",
+    ],
+    parraph:
+      "Muy pronto, estaré lanzando nuevos proyectos y demostrando mi habilidad para crear experiencias en línea únicas y entretenidas. Además, podrás ver mi capacidad para diseñar y desarrollar aplicaciones empresariales complejas, y cómo las tecnologías como Next.js, Tailwind CSS y Firebase me permiten crear soluciones tecnológicas innovadoras. ¡Manténte atento para descubrir más acerca de mi trabajo y cómo puedo ayudar a llevar tus ideas al siguiente nivel!",
+    links: ["http://github.com/incmplir/", "http://github.com/incmplir/"],
+  },
+  {
+    title: "Bebé",
+    page: "bebe",
+    badge: ["vite", "Tailwindcss"],
+    description: "Un proyecto personal desarrollado solo por diversión.",
+    bgProyect: [proyectBebe],
+    bgCard: [
+      "https://github.com/Incmplir/landing-page/blob/main/proyectBebe-0.png?raw=true",
+    ],
+    parraph:
+      "Proyecto Bebé - Desarrollado por pura diversión en mis ratos libres, Bebé es un proyecto chistoso que muestra a dos bebés luchando en un video 3D mientras una canción emocionante suena de fondo. Al principio, los usuarios son recibidos con una pantalla en negro simple con un botón en el centro. Al hacer clic en el botón, el video 3D aparece con dos bebés luchando mientras otros bebés bailan en el fondo. Este proyecto fue desarrollado usando Vite y Tailwind CSS para crear la pantalla inicial y agregar estilo a la página en general. La simplicidad de la pantalla inicial aumenta el efecto cómico del video 3D que aparece después. Si bien este proyecto fue creado por pura diversión, demuestra mi habilidad para usar herramientas de desarrollo para crear una experiencia única en línea. Además, muestra mi capacidad para crear un proyecto chistoso y entretenido que puede atraer a un público amplio. En resumen, el proyecto Bebé es una adición divertida y creativa a mi portafolio. Muestra mi habilidad para crear una experiencia en línea única y entretenida utilizando herramientas de desarrollo como Vite y Tailwind CSS.",
+    links: [
+      "https://bebe-vite.vercel.app/",
+      "https://github.com/Incmplir/bebe-vite",
+    ],
+  },
+  {
+    title: "Portafolio",
+    page: "portafolio",
+    badge: ["Next.js 13", "Tailwindcss"],
+    description:
+      "Mi primer proyecto actualizado constantemente con nuevas tecnologías.",
+    bgProyect: [proyectPortafolio],
+    bgCard: [
+      "https://github.com/Incmplir/landing-page/blob/main/proyectPortafolio-0.png?raw=true",
+    ],
+    parraph:
+      "Mi portafolio es una muestra de mi habilidad para crear aplicaciones web dinámicas y responsivas utilizando las últimas tecnologías de desarrollo. Construido con Next.js y Tailwindcss, presenta una interfaz de usuario limpia y moderna que se adapta perfectamente a cualquier dispositivo. Además, gracias a la implementación de páginas dinámicas y el fetching de datos, los usuarios pueden navegar sin problemas a través de diferentes secciones del sitio y acceder a información detallada sobre mis proyectos. Entre ellos se encuentran Bebé, un proyecto chistoso que demuestra mi capacidad para crear experiencias en línea únicas y entretenidas, y TamizLA, una solución tecnológica para profesionales laboratoristas que destaca mi habilidad para diseñar y desarrollar aplicaciones empresariales complejas. En resumen, mi portafolio es una muestra del alcance y la calidad de mi trabajo como desarrollador web.",
+    links: [
+      "http://christian-lisantti.netlify.app/",
+      "https://github.com/Incmplir/portfolio-next",
+    ],
+  },
+  {
+    title: "Tamíz.LA",
+    page: "tamiz la",
+    badge: ["Next.js", "Tailwindcss", "Supabase"],
+    description:
+      "Un proyecto ambicioso que llevo desarrollando desde hace un tiempo.",
+    bgProyect: [proyectTamizLA],
+    bgCard: [
+      "https://github.com/Incmplir/landing-page/blob/main/proyectTamizLA-0.png?raw=true",
+    ],
+    parraph:
+      "Proyecto TamizLA - Este es un proyecto ambicioso que he estado desarrollando durante algún tiempo. TamizLA es una solución tecnológica para profesionales laboratoristas que buscan mejorar la eficiencia en su trabajo. Al utilizar tecnologías modernas como Next.js, Tailwind CSS, Auth0 y Firebase, he creado un sistema que incluye características como material de estudio, seguimiento de ensayos, informes mensuales y más. Con TamizLA, los profesionales laboratoristas pueden ahorrar tiempo y aumentar la precisión en su trabajo, lo que resulta en una mayor satisfacción del cliente y una mayor eficiencia en general. Estoy emocionado de agregar TamizLA a mi portafolio, ya que demuestra mi capacidad para crear soluciones tecnológicas útiles y efectivas para problemas del mundo real.",
+    links: [
+      "http://tamiz-la.vercel.app/",
+      "https://github.com/Incmplir/tamizla-next",
+    ],
+  },
+  {
+    title: "Memeate",
+    page: "memeate",
+    badge: ["Vite", "Tailwindcss", "CodeSandbox"],
+    description: "Otro proyecto Gracioso desarrollado por los memes.",
+    bgProyect: [proyectMemeate],
+    bgCard: [proyectMemeate],
+    parraph:
+      "Proyecto Memeate - Desarrollado con pasión y creatividad en mis ratos libres, Memeate es un proyecto divertido que te permite crear y descargar memes personalizados de forma sencilla. Utilizando herramientas como Vite, Tailwind CSS y CodeSandbox, este proyecto ofrece una experiencia interactiva donde los usuarios pueden seleccionar plantillas de memes populares, agregar su propio texto personalizado y luego descargar los memes creados. Al ingresar al sitio, los usuarios son recibidos con una interfaz intuitiva y amigable. Se les presenta una selección de plantillas de memes, organizadas por categorías para facilitar la búsqueda. Al elegir una plantilla, se muestra una vista previa del meme con los campos de texto correspondientes. Los usuarios pueden personalizar el texto, agregar sus propias ideas ingeniosas y crear memes únicos. Una vez que los usuarios han creado su meme personalizado, tienen la opción de descargarlo en su dispositivo. Memeate proporciona un botón de descarga que les permite guardar el meme como una imagen en formato PNG o JPEG. Esto permite a los usuarios compartir sus memes en redes sociales, enviarlos a amigos o simplemente guardarlos como recuerdo. Además de la función de creación y descarga de memes, el proyecto Memeate cuenta con una dosis adicional de humor. A lo largo de la experiencia, se incluyen chistes entretenidos y divertidos, que mantienen el ambiente ligero y entretenido. Estos chistes se presentan de forma interactiva, sorprendiendo a los usuarios en momentos estratégicos y añadiendo un toque de diversión extra a la experiencia de creación de memes. El uso de CodeSandbox como entorno de desarrollo permite una colaboración sencilla y un fácil acceso al proyecto. Esto significa que los usuarios pueden disfrutar de Memeate en línea, sin necesidad de instalar nada en su dispositivo. La combinación de Vite y Tailwind CSS asegura un rendimiento óptimo y un diseño moderno y atractivo, proporcionando una experiencia de usuario fluida y agradable. En resumen, el proyecto Memeate es una adición creativa y entretenida a mi portafolio. Muestra mi habilidad para desarrollar aplicaciones web divertidas y útiles, aprovechando herramientas como Vite, Tailwind CSS y CodeSandbox. Además, la inclusión de chistes, la capacidad de personalizar los memes y la opción de descargarlos ofrecen una experiencia interactiva y agradable para los usuarios. Memeate es el lugar perfecto para dejar volar la imaginación, crear memes que despierten risas y tener la posibilidad de guardarlos como recuerdos o compartirlos con el mundo.",
+    links: [
+      "https://memeate-rilpmcuni.vercel.app/",
+      "https://github.com/Rilpmcuni/Memeate",
+    ],
+  },
 ];
--- src/layouts/Jumbotron.tsx
+++ src/layouts/Jumbotron.tsx
@@ -2,50 +2,47 @@ import Image from "next/image";
 import webp from "@/src/assets/image.webp";
 
 const Jumbotron = () => {
-    return (
-        <>
-            <section className="flex sm:flex-row flex-col w-11/12 h-[70vh] z-10 items-center justify-evenly sm:my-12 my-6">
-                <div className="flex flex-col mb-12">
-                    <h1 className="text-brand text-5xl font-black">
-                        Fabian Lisantti
-                    </h1>
-                    <h2 className="text-2xl">Desarrollador Frontend</h2>
-                    <p className="">
-                        Desarrollador Frontend buscando ideas y proyectos para
-                        mi mismo u otros.
-                    </p>
-                </div>
-                <div className="flex flex-col">
-                    <div className="container-logo-1 bg-text/80">
-                        <div className="container-logo-2 bg-brand/80">
-                            <div className="container-logo-3">
-                                <Image
-                                    height={400}
-                                    width={400}
-                                    priority={true}
-                                    className="frog h-auto w-11/12"
-                                    src={webp}
-                                    alt={"Fabian Lisantti"}
-                                ></Image>
-                            </div>
-                        </div>
-                    </div>
-                    <figure className="text-end z-50 -mt-28 bg-bg rounded-2xl p-4">
-                        <blockquote className="blockquote text-text/80 bg-bg-100">
-                            <p>
-                                A veces quisiera tirar la toalla, pero luego...
-                                ¿Con que me seco?
-                            </p>
-                            <figcaption className="blockquote-footer text-brand">
-                                Frases que Nadie dijo{" "}
-                                <cite title="Source Title">nunca</cite>
-                            </figcaption>
-                        </blockquote>
-                    </figure>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="flex sm:flex-row flex-col w-11/12 h-[70vh] z-10 items-center justify-evenly sm:my-12 my-6">
+        <div className="flex flex-col mb-12">
+          <h1 className="text-brand text-5xl font-black">Fabian Lisantti</h1>
+          <h2 className="text-2xl">Desarrollador Frontend</h2>
+          <p className="">
+            Desarrollador Frontend buscando ideas y proyectos para mi mismo u
+            otros.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <div className="container-logo-1 bg-text/80">
+            <div className="container-logo-2 bg-brand/80">
+              <div className="container-logo-3">
+                <Image
+                  height={400}
+                  width={400}
+                  priority={true}
+                  className="frog h-auto w-11/12"
+                  src={webp}
+                  alt={"Fabian Lisantti"}
+                ></Image>
+              </div>
+            </div>
+          </div>
+          <figure className="text-end z-50 -mt-28 bg-bg rounded-2xl p-4">
+            <blockquote className="blockquote text-text/80 bg-bg-100">
+              <p>
+                A veces quisiera tirar la toalla, pero luego... ¿Con que me
+                seco?
+              </p>
+              <figcaption className="blockquote-footer text-brand">
+                Frases que Nadie dijo <cite title="Source Title">nunca</cite>
+              </figcaption>
+            </blockquote>
+          </figure>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Jumbotron;
